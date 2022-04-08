ecba022df6c84046009eab198444ecbd1ebd44bb
samanthadotdesign <samleeml@gmail.com>
2022-04-09 07:31:54 +0800

    initial commit with dependencies

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-auto';
+import { resolve } from 'path';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		vite: {
+			resolve: {
+				alias: {
+					// these are the aliases and paths to them
+					'@assets': resolve('./src/constants'),
+					'@stores': resolve('./src/stores'),
+					'@components': resolve('./src/components'),
+					'@utils': resolve('./src/utils')
+				}
+			}
+		},
+		adapter: adapter()
+	}
+};
+
+export default config;
